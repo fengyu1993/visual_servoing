3b24ccf8c398ac31cdaf4c55eb5bb448f55f5a25
Yuhan Chen <chenyuhan19930920@163.com>
2022-10-10 20:03:39 +0800

    cyh

---
+++ franka_ros_learn/franka_example_controllers_cyh/include/franka_example_controllers_cyh/cartesian_pose_example_controller_cyh.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <array>
+#include <memory>
+#include <string>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <franka_hw/franka_state_interface.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+
+#include <franka_hw/franka_cartesian_command_interface.h>
+
+namespace franka_example_controllers_cyh{
+    class CartesianPoseExampleController_cyh : public 
+        controller_interface::MultiInterfaceController<franka_hw::FrankaPoseCartesianInterface,
+                                                            franka_hw::FrankaStateInterface>
+    {
+        public:
+        bool init(hardware_interface::RobotHW* robot_hardware, ros::NodeHandle& node_handle) override;
+        void starting(const ros::Time&) override;
+        void update(const ros::Time&, const ros::Duration& period) override;
+
+        private:
+        franka_hw::FrankaPoseCartesianInterface* cartesian_pose_interface_;
+        std::unique_ptr<franka_hw::FrankaCartesianPoseHandle> cartesian_pose_handle_;
+        ros::Duration elapsed_time_;
+        std::array<double, 16> initial_pose_{};
+    };
+}
