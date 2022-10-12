fe5aea2bb09d4e5377112a33fed2697625577b0d
Yuhan Chen <chenyuhan19930920@163.com>
2022-10-12 19:25:24 +0800

    model_example

---
+++ franka_ros_learn/franka_example_controllers_cyh/include/franka_example_controllers_cyh/model_example_controller_cyh.h
@@ -0,0 +1,30 @@
+#pragma once 
+
+#include <memory>
+#include <string>
+
+#include <controller_interface/multi_interface_controller.h>
+#include <hardware_interface/robot_hw.h>
+#include <ros/node_handle.h>
+#include <ros/time.h>
+
+#include <franka_hw/franka_model_interface.h>
+#include <franka_hw/franka_state_interface.h>
+#include <franka_hw/trigger_rate.h>
+
+namespace franka_example_controllers_cyh{
+    class ModelExampleController_cyh : public controller_interface::MultiInterfaceController<franka_hw::FrankaModelInterface,
+                                                            franka_hw::FrankaStateInterface> 
+    {
+        public:
+        bool init(hardware_interface::RobotHW* robot_hw, ros::NodeHandle& node_handle) override;
+        void update(const ros::Time&, const ros::Duration&) override;
+
+        private:
+        franka_hw::FrankaStateInterface* franka_state_interface_;
+        std::unique_ptr<franka_hw::FrankaStateHandle> franka_state_handle_;
+        franka_hw::FrankaModelInterface* model_interface_;
+        std::unique_ptr<franka_hw::FrankaModelHandle> model_handle_;
+        franka_hw::TriggerRate rate_trigger_{1.0};
+    };
+}
