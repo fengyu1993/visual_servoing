d015b801b08f9202db802fd4198669f6bf4ccdd1
Yuhan Chen <chenyuhan19930920@163.com>
2022-10-24 23:03:23 +0800

    cyh

--- franka_ros_learn/franka_moveit_cyh/src/track_marker_cyh.cpp
+++ franka_ros_learn/franka_moveit_cyh/src/track_marker_cyh.cpp
@@ -6,39 +6,36 @@
 #include <moveit_msgs/CollisionObject.h>
 #include <moveit_visual_tools/moveit_visual_tools.h>
 
-// moveit::planning_interface::MoveGroupInterface move_group_interface("panda_arm");
-// moveit::planning_interface::MoveGroupInterface::Plan my_plan;
-
-void track_marker_Callback(const geometry_msgs::Pose::ConstPtr& target_pose)
-{
-    // move_group_interface.setPoseTarget(*target_pose); 
-    
-    // move_group_interface.plan(my_plan);
-
-    // move_group_interface.move();
-    std::ostringstream s;
-    
-    ROS_INFO_STREAM( s.str() << ": pose changed"
-                << "\nposition = "
-                << target_pose->position.x
-                << ", " << target_pose->position.y
-                << ", " << target_pose->position.z
-                << "\norientation = "
-                << target_pose->orientation.w
-                << ", " << target_pose->orientation.x
-                << ", " << target_pose->orientation.y
-                << ", " << target_pose->orientation.z);  
-}
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "track_marker_cyh");
+
+    ros::NodeHandle node_handle;
+
+    ros::AsyncSpinner spinner(1);
+    spinner.start();
+
+    static const std::string PLANNING_GROUP = "panda_arm";
     
-    ros::NodeHandle nh;
+    moveit::planning_interface::MoveGroupInterface move_group_interface(PLANNING_GROUP);
 
-    ros::Subscriber sub = nh.subscribe("target_pose", 100, track_marker_Callback);
+    moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
-    ros::spin();
+    const moveit::core::JointModelGroup* joint_model_group = move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
+    // node_handle.getParam("target_pose", joint_names)
     return 0;   
 }
+
+
+    // ROS_INFO_STREAM( s.str() << ": pose changed"
+    //             << "\nposition = "
+    //             << target_pose->position.x
+    //             << ", " << target_pose->position.y
+    //             << ", " << target_pose->position.z
+    //             << "\norientation = "
+    //             << target_pose->orientation.w
+    //             << ", " << target_pose->orientation.x
+    //             << ", " << target_pose->orientation.y
+    //             << ", " << target_pose->orientation.z);  
