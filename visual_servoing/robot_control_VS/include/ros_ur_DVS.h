fc4576dad303cb7b60f8b1c65c017cbe91925d7b
fengyu1993 <chenyuhan19930920@163.com>
2024-10-22 20:13:32 +0800

    cyh

--- visual_servoing/robot_control_VS/include/ros_ur_DVS.h
+++ visual_servoing/robot_control_VS/include/ros_ur_DVS.h
@@ -1,25 +1,19 @@
-#ifndef ROS_DVS
-#define ROS_DVS
+#ifndef ROS_UR_DVS
+#define ROS_UR_DVS
 
 #include <iostream>
 #include <opencv2/opencv.hpp>
 #include <ros/ros.h>
 #include <sensor_msgs/Image.h>
-#include <sensor_msgs/CameraInfo.h>
 #include <geometry_msgs/Twist.h>
 #include <message_filters/subscriber.h>
 #include <message_filters/synchronizer.h>
 #include <message_filters/time_synchronizer.h>
 #include <cv_bridge/cv_bridge.h>
 #include <tf/transform_listener.h>
-#include <librealsense2/rs.hpp>
 #include <controller_manager_msgs/SwitchController.h>
 #include <controller_manager_msgs/ListControllers.h>
-#include <map>
-#include <string>
-#include <moveit/move_group_interface/move_group_interface.h>
-#include <thread>
-#include "polarimetric_visual_servoing.h"
+#include "direct_visual_servoing.h"
 #include <actionlib/client/simple_action_client.h>
 #include <control_msgs/FollowJointTrajectoryAction.h>
 #include "control_switcher_ur.h"
@@ -31,46 +25,45 @@ using namespace message_filters;
 
 typedef actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> Client;
 
-
-class Ros_DVS 
+class Ros_ur_DVS 
 {
-    // protected:
-    //     ros::NodeHandle                         nh_; 
-    //     message_filters::Subscriber<Image>      image_polar_sub_;
-    //     message_filters::Subscriber<Image>      image_depth_sub_;
-    //     actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> *client;
-    //     control_msgs::FollowJointTrajectoryGoal goal;
-    //     TimeSynchronizer<Image, Image>          *sync_;
-    //     tf::TransformListener                   listener_pose_;
-    //     Mat                                     effector_velocity_base_;
-    //     ros::Publisher                          pub_twist_; 
-
-    // public:
-    //     Polarimetric_Visual_Servoing *PVS;
-    //     double              control_rate_;
-    //     bool                flag_success_;
-    //     Mat                 joint_angle_initial_VS_;
-    //     bool                start_PVS;
-    //     ControlSwitcher_UR control_switcher_;
-    //     string name_link0_, name_camera_frame_, name_effector_;
+    protected:
+        ros::NodeHandle                         nh_; 
+        message_filters::Subscriber<Image>      image_color_sub_;
+        message_filters::Subscriber<Image>      image_depth_sub_;
+        actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction> *client;
+        control_msgs::FollowJointTrajectoryGoal goal;
+        TimeSynchronizer<Image, Image>          *sync_;
+        tf::TransformListener                   listener_pose_;
+        Mat                                     effector_velocity_base_;
+        ros::Publisher                          pub_twist_; 
 
     public:
-        Ros_DVS();
-    //     void Callback(const ImageConstPtr& image_color_msg, const ImageConstPtr& image_depth_msg);     
-    //     void get_parameters_resolution(int& resolution_x, int& resolution_y);
-    //     Mat get_parameter_Matrix(string str, int row, int col);
-    //     void initialize_time_sync();
-    //     void get_image_data_convert(const ImageConstPtr& image_polor_msg, const ImageConstPtr& image_depth_msg, Mat& polar_O_new, Mat& polar_A_new, Mat& polar_Phi_new, Mat& depth_img);
-    //     void polar_image_operate(Mat& img_polar, Mat& polar_O_new, Mat& polar_A_new, Mat& polar_Phi_new);
-    //     Mat depth_image_operate(Mat& image_depth);
-    //     Mat get_T(tf::StampedTransform  transform);
-    //     void get_parameters_PVS(double& lambda, double& epsilon, double& eta, double& phi_pol, double& k, Mat& polar_O_desired, Mat& polar_A_desired, Mat& polar_Phi_desired, Mat& image_depth_desired, Mat& camera_intrinsic, Mat& pose_desired);    
-    //     void robot_move_to_target_joint_angle(std::vector<double> joint_group_positions_target);
-    //     void get_camera_effector_pose(Mat& effector_to_base, Mat& camera_to_effector);
-    //     Mat get_camera_pose();
-    //     Mat get_effector_velocity(Mat camera_velocity, Mat effector_to_camera);
-    //     Mat velocity_effector_to_base(Mat velocity, Mat effector_to_base);
-    //     void twist_publist(Mat camera_velocity);
+        Direct_Visual_Servoing *DVS;
+        double              control_rate_;
+        bool                flag_success_;
+        Mat                 joint_angle_initial_VS_;
+        bool                start_DVS;
+        ControlSwitcher_UR control_switcher_;
+        string name_link0_, name_camera_frame_, name_effector_;
+
+    public:
+        Ros_ur_DVS();
+        void Callback(const ImageConstPtr& image_color_msg, const ImageConstPtr& image_depth_msg);     
+        void get_parameters_resolution(int& resolution_x, int& resolution_y);
+        Mat get_parameter_Matrix(string str, int row, int col);
+        void initialize_time_sync();
+        void get_image_data_convert(const ImageConstPtr& image_color_msg, const ImageConstPtr& image_depth_msg, Mat& color_img, Mat& depth_img);
+        Mat rgb_image_operate(Mat& image_rgb);
+        Mat depth_image_operate(Mat& image_depth);
+        Mat get_T(tf::StampedTransform  transform);
+        void get_parameters_DVS(double& lambda, double& epsilon, Mat& image_gray_desired, Mat& image_depth_desired, Mat& camera_intrinsic, Mat& pose_desired);    
+        void robot_move_to_target_joint_angle(std::vector<double> joint_group_positions_target);
+        void get_camera_effector_pose(Mat& effector_to_base, Mat& camera_to_effector);
+        Mat get_camera_pose();
+        Mat get_effector_velocity(Mat camera_velocity, Mat effector_to_camera);
+        Mat velocity_effector_to_base(Mat velocity, Mat effector_to_base);
+        void twist_publist(Mat camera_velocity);
 };
 
 #endif
--- visual_servoing/robot_control_VS/src/ur_control_DVS.cpp
+++ visual_servoing/robot_control_VS/src/ur_control_DVS.cpp
@@ -1,44 +1,40 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <ros/ros.h>
-#include "direct_visual_servoing.h"
 #include "ros_ur_DVS.h"
 #include "key.h"
 
 int main(int argc, char** argv)
 {
-    // // 准备
-    // ros::init(argc, argv, "PVS");  
-    // ros::AsyncSpinner spinner(1);
-    // spinner.start();  
-    // cout << "Move to initial pose ... " << endl;
-    // cout << "Press Enter to start the experiment ..." << endl;
-    // cin.ignore();
-    // Ros_PVS PVS_control;
-    // // 机械臂移动到起始位姿
-    // PVS_control.control_switcher_.switch_controllers("position", "twist");
-    // std::vector<double> joint_group_positions_start= {-73.9*CV_PI/180.0, -86.36*CV_PI/180.0, -98.73*CV_PI/180.0, -96.0*CV_PI/180.0, 72.22*CV_PI/180.0, -176.42*CV_PI/180.0};
-    // cout << "Move to initial pose ... " << endl;
-    // cout << "Press Enter to start..." << endl;
-    // cin.ignore();
-    // PVS_control.robot_move_to_target_joint_angle(joint_group_positions_start);
-    // cout << "Move to initial pose finished " << endl;
-    // // 机械臂移动到初始伺服位姿
-    // cout << "Move to initial VS pose ... " << endl;
-    // cout << "Press Enter to start..." << endl;
-    // cin.ignore();
-    // PVS_control.robot_move_to_target_joint_angle(PVS_control.joint_angle_initial_VS_);
-    // spinner.stop();
-    // // 转换控制器
-    // PVS_control.control_switcher_.switch_controllers("twist", "position");
-    // // 视觉伺服控制
-    // cout << "Start visual servoing control ... " << endl;
-    // cout << "Press Enter to start..." << endl;
-    // cin.ignore();
-    // // PVS_control.initialize_time_sync();
-    // // PVS_control.start_PVS = true;
-    // ros::Rate loop_rate(PVS_control.control_rate_);
-    // int num = 0; 
+    // 准备
+    ros::init(argc, argv, "DVS");  
+    ros::AsyncSpinner spinner(1);
+    spinner.start();  
+    cout << "Move to initial pose ... " << endl;
+    cout << "Press Enter to start the experiment ..." << endl;
+    cin.ignore();
+    Ros_ur_DVS DVS_control;
+    // 机械臂移动到起始位姿
+    DVS_control.control_switcher_.switch_controllers("position", "twist");
+    std::vector<double> joint_group_positions_start= {-73.9*CV_PI/180.0, -86.36*CV_PI/180.0, -98.73*CV_PI/180.0, -96.0*CV_PI/180.0, 72.22*CV_PI/180.0, -176.42*CV_PI/180.0};
+    cout << "Move to initial pose ... " << endl;
+    cout << "Press Enter to start..." << endl;
+    cin.ignore();
+    DVS_control.robot_move_to_target_joint_angle(joint_group_positions_start);
+    cout << "Move to initial pose finished " << endl;
+    // 机械臂移动到初始伺服位姿
+    cout << "Move to initial VS pose ... " << endl;
+    cout << "Press Enter to start..." << endl;
+    cin.ignore();
+    DVS_control.robot_move_to_target_joint_angle(DVS_control.joint_angle_initial_VS_);
+    spinner.stop();
+    // 转换控制器
+    DVS_control.control_switcher_.switch_controllers("twist", "position");
+    // 视觉伺服控制
+    cout << "Start visual servoing control ... " << endl;
+    cout << "Press Enter to start..." << endl;
+    cin.ignore();
+    // DVS_control.initialize_time_sync();
+    // DVS_control.start_DVS = true;
+    ros::Rate loop_rate(DVS_control.control_rate_);
+    int num = 0; 
 
     // auto KBC = Keyboard_ctrl();
     // double vel_linear = 0.01;
@@ -117,13 +113,13 @@ int main(int argc, char** argv)
     //             break;   
     //     }
 
-    //     PVS_control.twist_publist(camera_velocity);
+    //     DVS_control.twist_publist(camera_velocity);
     //     // 休息
     //     loop_rate.sleep();
     //     // try{
-    //     //     if(PVS_control.flag_success_){
+    //     //     if(DVS_control.flag_success_){
     //     //         ROS_INFO("visual servoing success");
-    //     //         PVS_control.start_PVS = false;
+    //     //         DVS_control.start_DVS = false;
     //     //         break;
     //     //     }else{
     //     //         ros::spinOnce();
@@ -134,13 +130,13 @@ int main(int argc, char** argv)
     //     // }
     // }
     // // 转换控制器
-    // PVS_control.control_switcher_.switch_controllers("position", "twist");
+    // DVS_control.control_switcher_.switch_controllers("position", "twist");
     // // 机械臂移动到起始位姿
     // cout << "Move to work position ... " << endl;
     // cout << "Press Enter to start..." << endl;
     // cin.ignore();
     // spinner.start();
-    // PVS_control.robot_move_to_target_joint_angle(joint_group_positions_start);
+    // DVS_control.robot_move_to_target_joint_angle(joint_group_positions_start);
     // // 结束
     // return 0;
 }
